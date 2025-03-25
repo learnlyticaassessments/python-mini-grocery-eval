c1a8bf60ca7e7c9e0eab067072dc7b8a696fcc25
learnlyticaassessments <assessments@learnlytica.com>
2025-03-25 17:16:55 +0530

    Update test_cases.py

--- evaluate/test_cases.py
+++ evaluate/test_cases.py
@@ -1,4 +1,8 @@
 import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "student_repos")))
+
 from student import GroceryManager
 
 @pytest.fixture
